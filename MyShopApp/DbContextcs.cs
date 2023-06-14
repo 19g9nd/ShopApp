90d64de97265dcb3c8db767fc383db47a94f5884
Diana Ahadova <ahadova2017@gmail.com>
2023-06-14 23:32:46 +0400

    First commit

---
+++ MyShopApp/DbContextcs.cs
@@ -0,0 +1,29 @@
+﻿using ConsoleApp5.Classes;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class DbContextcs : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = $"Server=localhost;Database=MyShopDb;Trusted_Connection=True;TrustServerCertificate=True;";
+            optionsBuilder.UseSqlServer(connectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
---
+++ MyShopApp/InstallCommands.cmd
@@ -0,0 +1,7 @@
+﻿dotnet tool install --global dotnet-ef
+
+dotnet add package Microsoft.EntityFrameworkCore
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+dotnet add package Microsoft.EntityFrameworkCore.Design
+dotnet add package SimpleInjector
+dotnet add package Dapper
